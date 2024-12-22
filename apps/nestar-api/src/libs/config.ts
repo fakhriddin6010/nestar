156c97b87fe63ccc363a5ec3b0e617c7c24e43cb
fakhriddin <faxa99deni@gmail.com>
2024-12-22 17:08:19 +0900

    feat: create subscribe , unsubscribe, getMemberFollowings and getMemberFollwers GraphQl APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -14,8 +14,8 @@ export const availablePropertySorts = [
 	'propertyPrice',
 ];
 
-export const availableArticleSort = ['createdAt', 'updatedAt', 'articleLikes','articleViews']
-export const availableCommentSorts= ['createdAt', 'updatedAt']
+export const availableArticleSort = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
+export const availableCommentSorts = ['createdAt', 'updatedAt'];
 /** IMAGE CONFIGURATION **/
 import * as path from 'path';
 import { v4 as uuidv4 } from 'uuid';
@@ -36,5 +36,23 @@ export const lookupMember = {
 		localField: 'memberId',
 		foreignField: '_id',
 		as: 'memberData',
+	},
+};
+
+export const lookupFollowingData = {
+	$lookup: {
+		from: 'members',          // 2 members collectiondan
+		localField: 'followingId', //1 following id orqali
+		foreignField: '_id',       //3 _id teng bolgan qiymatini hosil qilib uni 
+		as: 'followingData',     // 4 followingdataga hosil qilyabmiz
+	},
+};
+
+export const lookupFollowerData = {
+	$lookup: {
+		from: 'members',
+		localField: 'followerId',
+		foreignField: '_id',
+		as: 'followerData',
 	},
 };
