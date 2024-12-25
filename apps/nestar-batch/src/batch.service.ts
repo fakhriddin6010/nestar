306b9abeb4a0b29a8308ea34547342d0d4490720
fakhriddin <faxa99deni@gmail.com>
2024-12-25 09:45:28 +0900

    feat: create batchRollback, batchProperties and batchAgents jobSchedulers logic

--- apps/nestar-batch/src/batch.service.ts
+++ apps/nestar-batch/src/batch.service.ts
@@ -2,7 +2,17 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BatchService {
-  getHello(): string {
-    return 'Welcome to Nestar Batch Server!';
-  }
+	getHello(): string {
+		return 'Welcome to Nestar Batch Server!';
+	}
+
+	public async batchRollback(): Promise<void> {
+		console.log('batchRollback');
+	}
+	public async batchProperties(): Promise<void> {
+		console.log('batchProperties');
+	}
+	public async batchAgents(): Promise<void> {
+		console.log('batchAgents');
+	}
 }
